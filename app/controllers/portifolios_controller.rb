642a2b0c6a7a6c45560341c55c48da2c2840d1d5
WaseekSenju <waseeq69ahmed@gmail.com>
2023-10-18 18:36:13 +0500

    update portifolio action added

--- app/controllers/portifolios_controller.rb
+++ app/controllers/portifolios_controller.rb
@@ -18,5 +18,20 @@ class PortifoliosController < ApplicationController
           end
         end
       end
+    
+    def edit
+        @portfolio_item = Portifolio.find(params[:id])
+    end
+
+    def update
+        @portfolio_item = Portifolio.find(params[:id])
+        respond_to do |format|
+          if @portfolio_item.update(params.require(:portifolio).permit(:title,:subtitle, :body))
+            format.html { redirect_to portifolios_path, notice: "portfolio_item was successfully updated." }    
+          else
+            format.html { render :edit, status: :unprocessable_entity }
+          end
+        end
+      end
 
 end
