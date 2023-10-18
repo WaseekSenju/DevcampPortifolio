62effedc2de169d88978eb0f46cc56ca3f94ff12
WaseekSenju <waseeq69ahmed@gmail.com>
2023-10-18 17:59:19 +0500

    new portifolio feature added

--- app/controllers/portifolios_controller.rb
+++ app/controllers/portifolios_controller.rb
@@ -3,4 +3,20 @@ class PortifoliosController < ApplicationController
         @portfolio_items = Portifolio.all 
     end
 
+    def new 
+        @portfolio_item = Portifolio.new 
+    end
+
+    def create
+        @portfolio_item = Portifolio.new(params.require(:portifolio).permit(:title,:subtitle, :body))
+    
+        respond_to do |format|
+          if @portfolio_item.save
+            format.html { redirect_to portifolios_path, notice: "portfolio_item was successfully created." }
+          else
+            format.html { render :new, status: :unprocessable_entity }
+          end
+        end
+      end
+
 end
